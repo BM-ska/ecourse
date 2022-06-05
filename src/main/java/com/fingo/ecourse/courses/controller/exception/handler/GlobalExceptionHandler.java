24fb2cd2f6fcbcdf1d89a38543657a0d76e68bd6
PiotrStoklosa <piotrjstoklosa@gmail.com>
2022-06-05 22:18:42 +0200

    Add exception handling

---
+++ src/main/java/com/fingo/ecourse/courses/controller/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.fingo.ecourse.courses.controller.exception.handler;
+
+import com.fingo.ecourse.courses.controller.exception.model.CourseDuplicateException;
+import com.fingo.ecourse.courses.controller.exception.model.NotFoundException;
+import com.fingo.ecourse.courses.service.CourseService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    private static final Logger LOGGER = LogManager.getLogger(CourseService.class);
+
+    @ExceptionHandler(NotFoundException.class)
+    public final ResponseEntity<String> handleNotFound(Exception e) {
+        LOGGER.error("NotFoundException handler executed");
+        return new ResponseEntity<>(
+                e.getMessage(),
+                HttpStatus.NOT_FOUND
+        );
+    }
+
+    @ExceptionHandler(CourseDuplicateException.class)
+    public final ResponseEntity<String> handleCourseDuplicateException(Exception e) {
+        LOGGER.error("CourseDuplicate handler executed");
+        return new ResponseEntity<>(
+                e.getMessage(),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+}
