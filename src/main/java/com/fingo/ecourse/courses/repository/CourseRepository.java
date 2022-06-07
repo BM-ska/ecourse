efa55fec9143ff7899dcb7f315ab04915ef20592
PiotrStoklosa <piotrjstoklosa@gmail.com>
2022-06-07 15:04:52 +0200

    Add query to obtain courses by category

--- src/main/java/com/fingo/ecourse/courses/repository/CourseRepository.java
+++ src/main/java/com/fingo/ecourse/courses/repository/CourseRepository.java
@@ -2,8 +2,11 @@ package com.fingo.ecourse.courses.repository;
 
 import com.fingo.ecourse.courses.repository.model.CourseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -11,5 +14,9 @@ import java.util.Optional;
  */
 @Repository
 public interface CourseRepository extends JpaRepository<CourseEntity, Integer> {
+
     Optional<CourseEntity> findByCourseNameContainsIgnoreCase(String courseName);
+
+    @Query("SELECT c FROM CourseEntity c WHERE c.id_category = :id_category")
+    List<CourseEntity> retrieveByCategoryName(@Param("category_id") Integer categoryId);
 }
