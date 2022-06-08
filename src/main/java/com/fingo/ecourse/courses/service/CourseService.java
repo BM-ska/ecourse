a76441a247ece795cef13ab0ab36bad40dec08a8
PiotrStoklosa <piotrjstoklosa@gmail.com>
2022-06-08 21:33:33 +0200

    Create relation many to one in courses part 2

--- src/main/java/com/fingo/ecourse/courses/service/CourseService.java
+++ src/main/java/com/fingo/ecourse/courses/service/CourseService.java
@@ -1,5 +1,7 @@
 package com.fingo.ecourse.courses.service;
 
+import com.fingo.ecourse.categories.repository.CategoryRepository;
+import com.fingo.ecourse.categories.repository.model.CategoryEntity;
 import com.fingo.ecourse.courses.controller.exception.model.CourseDuplicateException;
 import com.fingo.ecourse.courses.controller.exception.model.NotFoundException;
 import com.fingo.ecourse.courses.repository.CourseRepository;
@@ -24,11 +26,30 @@ public class CourseService {
     private static final Logger LOGGER = LogManager.getLogger(CourseService.class);
 
     private final CourseRepository courseRepository;
+    private final CategoryRepository categoryRepository;
     private final ServiceRepositoryModelCourseMapper mapper = Mappers.getMapper(ServiceRepositoryModelCourseMapper.class);
 
     public Iterable<ServiceModelCourse> getAllCourses() {
         LOGGER.info("Get all courses");
         Iterable<CourseEntity> repositoryModelCourseEntityIterable = courseRepository.findAll();
+
+        List<ServiceModelCourse> tmp = new ArrayList<>();
+        repositoryModelCourseEntityIterable.forEach(x -> tmp.add(mapper.fromRepositoryToServiceModel(x)));
+
+        LOGGER.info("Courses: " + tmp);
+        LOGGER.info("Get all courses by category name successfully");
+
+        return tmp;
+    }
+
+    public Iterable<ServiceModelCourse> getAllCoursesByCategoryName(String categoryName) {
+        LOGGER.info("Get all courses by category name: " + categoryName);
+
+        CategoryEntity categoryEntity = categoryRepository.findAll()
+                .stream()
+                .filter(category -> category.getCategoryName().equals(categoryName))
+                .findFirst().orElseThrow();
+        List<CourseEntity> repositoryModelCourseEntityIterable = courseRepository.retrieveByCategoryName(categoryEntity);
 
         List<ServiceModelCourse> tmp = new ArrayList<>();
         repositoryModelCourseEntityIterable.forEach(x -> tmp.add(mapper.fromRepositoryToServiceModel(x)));
