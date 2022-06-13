efaebdddb94ced4df628038257137bb64d125e83
BM-ska <barbaramoczulska1a@gmail.com>
2022-06-13 13:04:00 +0200

    Add generate category buttons using list

--- frontend/ecourse/src/content/allCategories/ViewAllCategories.tsx
+++ frontend/ecourse/src/content/allCategories/ViewAllCategories.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import CategoryButton from './CategoryButton';
-import { Col, Row } from 'antd';
 import axios from 'axios';
+import { Col, Row} from 'antd';
 
 const boardStyle = {
   background: 'white',
@@ -11,26 +10,64 @@ const boardStyle = {
 
 var x: any;
 
-function updateCategories(){
-  return(
-  <h2 style={boardStyle}>
-      <Row>
-        <Col span={24}><CategoryButton buttonName="Liczby pierwsze" routingPath="/categories" /></Col>
-      </Row>
+var categoriesList = [{ categoryName: "Computer Science", id: 1 },
+                      { categoryName: "Maths", id: 2 },
+                      { categoryName: "Physics", id: 3 },
+                      { categoryName: "Computer Science1", id: 1 },
+                      { categoryName: "Maths1", id: 2 },
+                      { categoryName: "Physics1", id: 3 },
+                      { categoryName: "Computer Science2", id: 1 },
+                      { categoryName: "Maths2", id: 2 },
+                      { categoryName: "Physics2", id: 3 },
+                      { categoryName: "Physics", id: 3 },
+                      { categoryName: "Computer Science1", id: 1 },
+                      { categoryName: "Maths1", id: 2 },
+                      { categoryName: "Physics1", id: 3 },
+                      { categoryName: "Computer Science2", id: 1 },
+                      { categoryName: "Maths2", id: 2 },
+                      { categoryName: "Physics2", id: 3 },
+                      { categoryName: "Maths2", id: 2 },
+                      { categoryName: "Computer Science1", id: 1 },
+                      { categoryName: "Maths1", id: 2 },
+                      { categoryName: "Physics1", id: 3 },
+                      { categoryName: "Computer Science2", id: 1 },
+                      { categoryName: "Maths2", id: 2 },
+                      { categoryName: "Physics2", id: 3 }];
 
-    </h2>
+function updateCategories(){
+
+  const cols = [];
+  const colCount = categoriesList.length;
+  let colCode = '';
+  
+  for (let i = 0; i < colCount; i++) {
+    cols.push(
+      <Col key={i.toString()} span={6}>
+        <CategoryButton buttonName={categoriesList[i].categoryName} routingPath="/" />
+      </Col>,
+    );
+  }
+
+  return (
+    <>
+      <Row gutter={[16, 16]}>
+        {cols}
+      </Row>
+      </>
   );
-}
+};
 
 function getCategories() {
   axios.get('http://localhost:8080/api/v1/categories')
       .then(res => {
         x = res.data;
+        
+        console.log(x);
       })
 }
 
  function ViewAllCategories() {
-  getCategories();
+  //getCategories();
 
   return (
     <><>
@@ -46,7 +83,7 @@ function getCategories() {
       </h1>
     </><>
 
-        
+        {updateCategories()}
       </></>
     
   );
