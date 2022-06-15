ec783e2ad66acc7f37de6e73570a936846c7d752
BM-ska <barbaramoczulska1a@gmail.com>
2022-06-15 12:46:34 +0200

    Update categories

---
+++ frontend/ecourse/src/content/allCategories/UpdateCategories.tsx
@@ -0,0 +1,36 @@
+import { Col, Row } from "antd";
+import CategoryButton from "./CategoryButton";
+
+
+interface Category {
+    categoryArray:{
+      categoryName: string;
+      id: number;
+    }[]
+  }
+  
+function UpdateCategories(props: Category){
+    const cols = [];
+    const colCount = props.categoryArray.length;
+ 
+    console.log("bbbbb");
+    console.log(props.categoryArray);
+
+    for (let i = 0; i < colCount; i++) {
+        cols.push(
+        <Col key={i.toString()} span={6}>
+            <CategoryButton buttonName={props.categoryArray[i].categoryName} routingPath="/" />
+        </Col>,
+        );
+    }
+
+    console.log("ccc");
+    return (
+        <>
+        <Row gutter={[16, 16]}>
+            {cols}
+        </Row>
+        </>
+    );
+};
+export default UpdateCategories;
