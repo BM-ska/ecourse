38127b86873a32aa323a4866401cb31b43d34e6b
PiotrStoklosa <piotrjstoklosa@gmail.com>
2022-06-21 19:36:23 +0200

    Extract create buttons to separate components part II

---
+++ frontend/ecourse/src/create-course-form/IForm.tsx
@@ -0,0 +1,9 @@
+interface IForm {
+    categoryName: string;
+    courseName: string;
+    shortDescription?: string;
+    longDescription: string;
+    courseLink: string;
+}
+
+export default IForm;
