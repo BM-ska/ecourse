82650785ab57ca346290cb606f0216b209db924c
Basia <barbaramoczulska1a@gmail.com>
2023-09-09 13:58:01 +0200

    add

--- src/main/java/com/fingo/ecourse/courses/repository/CourseRepository.java
+++ src/main/java/com/fingo/ecourse/courses/repository/CourseRepository.java
@@ -1,10 +1,7 @@
 package com.fingo.ecourse.courses.repository;
 
-import com.fingo.ecourse.categories.repository.model.CategoryEntity;
 import com.fingo.ecourse.courses.repository.model.CourseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
